e5430ee5654b62f9d4be3b5c547096c4a4c6386e
Kirill <324505538+ck59dd7mwt-ctrl@users.noreply.github.com>
2026-09-18 21:29:07 +0500

    Prototype

--- ProjectGraph.cpp
+++ ProjectGraph.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std; 
 
 int main(){
-    vector<vector<int>>graph(4);
-    vector<int>rebra;
-    graph[0].push_back(1);
-    graph[0].push_back(3);
-    graph[1].push_back(2);
-    graph[2].push_back(3);
-    for(int i = 0; i < graph.size(); i++){
-        for(const auto& n : graph[i]){
-            cout << i << "->" << n << '\n';
-        }
-    }
+    struct Variable {
+        string name;
+        bool selfDepent;
+    };
+    struct Loop {
+        string condition;
+        vector<Variable> variables;
+    };
+    struct Func {
+        string name;
+        vector<Variable> input;
+        vector<Variable> variables;
+        vector<Loop> loops;
+        vector<Variable> Func_return;
+    };
+    Variable result;
+    result.name = "int result";
+    result.selfDepent = true;
+    Variable area;
+    area.name = "int area";
+    area.selfDepent = false;
+    Variable input_func;
+    input_func.name = "vector<int>& height";
+    input_func.selfDepent = false;
+
+
+    Loop loop_while;
+    loop_while.condition = "left < right";
+
+    loop_while.variables.push_back(area);
+    
+    
+    Func maxArea;
+    maxArea.name = "maxArea";
+    maxArea.variables.push_back(result);
+    maxArea.loops.push_back(loop_while);
+    maxArea.input.push_back(input_func);
+    maxArea.Func_return.push_back(result);
+
 }
